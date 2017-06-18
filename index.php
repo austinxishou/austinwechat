b2937721f2f3a9bf28c4405291e033c19c33429b
austindev <austinxishou@yahoo.com>
2017-06-18 10:19:57 +0800

    删除首页index.php debug日志；

--- index.php
+++ index.php
@@ -8,14 +8,11 @@ $host = $_SERVER['HTTP_HOST'];
 if (!empty($host)) {
 	$bindhost = pdo_fetch("SELECT * FROM ".tablename('site_multi')." WHERE bindhost = :bindhost", array(':bindhost' => $host));
 	if (!empty($bindhost)) {
-        WeUtility::logging('debug','index bindhost 入口');
-        echo("index 入口测试");
 		header("Location: ". $_W['siteroot'] . 'app/index.php?i='.$bindhost['uniacid'].'&t='.$bindhost['id']);
 		exit;
 	}
 }
 
-WeUtility::logging('debug','index 入口测试'.$_W['os']);
 if($_W['os'] == 'mobile' && (!empty($_GPC['i']) || !empty($_SERVER['QUERY_STRING']))) {
 	header('Location: ./app/index.php?' . $_SERVER['QUERY_STRING']);
 } else {
